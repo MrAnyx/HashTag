94c85998e141d7dd1846e9625257b1caba4c54e0
MrAnyx <1ptitigre@gmail.com>
2019-11-03 12:03:26 +0100

    [add] json file + sha256 + main page

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const { app, BrowserWindow, ipcMain, Menu } = require('electron')
 const path = require('path')
 const bcrypt = require("bcryptjs")
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const savesFile = new FileSync('json/saves.json')
+const saves = low(savesFile)
+
+const md5 = require("md5")
 
 let win
 
+saves.defaults({ historic: [], most_used: [] }).write()
+
 
 app.on('ready', () => {
-
 	win = new BrowserWindow({
 		width: 600,
 		height: 400,
@@ -19,14 +26,18 @@ app.on('ready', () => {
 	})
 
 	win.loadFile(path.join(__dirname, './templates/layout.html'))
-
 	win.on("ready-to-show", () => { win.show() })
-
 	win.on('closed', () => {
 		win = null
 	})
-
 })
+
+ipcMain.on("hash-md5", (event, args) => {
+	event.reply("hash-md5-reply", md5(args))
+})
+
+
+
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
